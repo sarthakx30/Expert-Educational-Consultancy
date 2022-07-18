f645e94c6d482d904b787abbc49817bd5475bb78
Archit0123 <architagarwal2002@gmail.com>
2022-07-18 19:04:05 +0530

    Fixed more minor bugs for deployment

--- projbackend/index.js
+++ projbackend/index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const app = require("./app");
 const dbConn = require("./config/dbConn");
 const _ = require("lodash"); //I dont understand why i have to use lodash here when i didnt have to in my previous(ecomm store proj).
+const PORT= process.env.PORT || 4679
 dbConn();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
 });
