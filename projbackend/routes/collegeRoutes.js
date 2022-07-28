30bd943f819e0ade7cc959ce041ca5cb788be21d
Archit0123 <architagarwal2002@gmail.com>
2022-07-28 20:01:52 +0530

    "testing login tokens"

--- projbackend/routes/collegeRoutes.js
+++ projbackend/routes/collegeRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { getColleges } = require("../controllers/collegeController");
+const {isLoggedIn}= require("../middlewares/userMiddleware")
 
-router.route("/colleges").get(getColleges);
+router.route("/colleges").get(isLoggedIn,getColleges);
 
 module.exports = router;
